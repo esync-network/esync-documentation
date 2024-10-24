fd166d7de59fce2bebcde8e9b3b48384157f6b76
eSync Developer <dev@ecredits.com>
2024-10-24 14:54:32 +0200

    add missing images and remove social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
 
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      // image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Home',
         logo: {
